cbd1e6b46e6902701138f045c877646abc62639d
Danielle Lee <daniellelee73@gmail.com>
2025-12-10 17:30:15 +0000

    feat: add imports to app component to allow for routing to homescreen, tracker and history pages

--- src/App.tsx
+++ src/App.tsx
@@ -1,20 +1,34 @@
+import { useState } from "react"
 import './index.css'
 import { BrowserRouter, Routes, Route } from "react-router-dom"
 import HomeScreen from "@/screens/HomeScreen"
-import TrackerScreen from '@/screens/TrackerScreen';
-
+import TrackerScreen from "@/screens/TrackerScreen"
+import HistoryScreen from "@/screens/HistoryScreen"
+import type { PeriodEntry } from "./types/period"
 
 function App() {
-return(
-  <>
-  <BrowserRouter>
-  <Routes>
+  const [periodEntries, setPeriodEntries] = useState<PeriodEntry[]>([])
+
+  return (
+    <BrowserRouter>
+      <Routes>
         <Route path="/" element={<HomeScreen />} />
-        <Route path="/tracker" element={<TrackerScreen />} />
+        <Route
+          path="/tracker"
+          element={
+            <TrackerScreen
+              periodEntries={periodEntries}
+              setPeriodEntries={setPeriodEntries}
+            />
+          }
+        />
+        <Route
+          path="/history"
+          element={<HistoryScreen entries={periodEntries} />}
+        />
       </Routes>
-  </BrowserRouter>
-  </>
-);
+    </BrowserRouter>
+  )
 }
 
 export default App
